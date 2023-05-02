241d6ba0dd3dd7e8862d32f525e0b62272cabe8e
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-05-02 16:26:32 +0300

    unused components removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/Header";
 import Weather from "./components/Weather";
 import Dropdown from "./components/Dropdown";
-import Footer from "./components/Footer";
 import { LocationProvider } from "./context/LocationContext";
 
 function App() {
@@ -11,12 +10,10 @@ function App() {
       <br />
       <Header />
       <br />
-
       <Weather />
       <br />
       <br />
       <Dropdown />
-      <Footer />
     </LocationProvider>
   );
 }
